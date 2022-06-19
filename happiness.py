e0d147d222b7e98f290f2ee4d835893158c1f8af
KatelynTan <85561762+KatelynTan@users.noreply.github.com>
2022-06-19 18:56:53 +0800

    Add files via upload

--- happiness.py
+++ happiness.py
@@ -42,7 +42,7 @@ st.dataframe(happiness_revised)
 code2 = '''merged_table = pd.concat([happiness_revised,world_happiness_21[list(happiness_revised.columns)+['Regional indicator']]])'''
 st.code(code2, language = 'python')
 st.dataframe(merged_table)
-st.markdown('Notice in the column "Regional indicator", there are a lot of "<NA>" instead of a region. After looking into this problem, I found out that for the 2021 dataset, they include a regional indicaator, whereas the other dataset does not. To solve this problem, I created a dictionary using a for loop:')
+st.markdown('Notice in the column "Regional indicator", there are a lot of "<NA>" instead of a region. After looking into this problem, I found out that for the 2021 dataset, they include a regional indicator, whereas the other dataset does not. To solve this problem, I created a dictionary using a for loop:')
 code3 = '''region_dict = {k:v for k,v in zip(world_happiness_21['Country name'],world_happiness_21['Regional indicator'])}
 region_dict'''
 code4 = region_dict = {k:v for k,v in zip(world_happiness_21['Country name'],world_happiness_21['Regional indicator'])}
@@ -87,6 +87,7 @@ scatterfig = px.scatter(merged_table, x= xscatter, y = yscatter, color = 'Region
 col6.plotly_chart(scatterfig)
 col5.markdown('This scatter plot allows you to compare the correlation between different happiness factors. We can also observe from this plot that 3 factors most associated with the life ladder are Log GDP per capita, healthy life expectancy at birth, and social support (to reach this conclusion, set "ladder score" on one axis and try assigning other variables to the other axis). Similarly, we can infer that the three variables that are most negatively correlated with the life ladder are generosity, perceptions of corruption, and year. These results further validate the conclusions we made from the heat map.')
 
+st.header('Comparing regions and countries')
 st.subheader('Interactive Choropleth Map')
 col9,col10 = st.columns([2,5])
 col9.markdown('Besides finding out the most associated variables to life ladder score and the least associated, you might also noticed that the different regions shown through the different colored dots seem to be concentrated. To better visualize the life ladder by region, I created a choropleth map in the shape of a globe.')
@@ -114,7 +115,8 @@ col11.markdown('From this box plot, we can see that there are not many outliers,
 
 st.subheader('Interactive Bar Chart (top 10 countries of each year)')
 def by_year(x):
-    return merged_table[merged_table['year'] == x].sort_values(by = ['Life Ladder'], ascending= False).head(10)
+       tempdf = merged_table[merged_table['year'] == x].copy()
+       return tempdf.sort_values(by = ['Life Ladder'], ascending= False).head(10).reset_index(drop=True)
 y05 = by_year(2005)
 y06 = by_year(2006)
 y07 = by_year(2007)
@@ -139,28 +141,29 @@ top_10_life_ladder = {}
 for i,x in enumerate(years):
     temp_fig = px.bar(x, x="Life Ladder", y="Country name", orientation='h',
            color ='Region', hover_name = 'Country name', title = titles[i], height = 600, width = 800)
+    temp_fig.update_layout(yaxis = {'tickvals':x.index, 'ticktext':x['Country name']})
     top_10_life_ladder.update({year_num[i]:temp_fig})
 col3,col4 = st.columns([2,5])
 yearoption = col3.selectbox('Select the year',year_num)
 col4.write(top_10_life_ladder[yearoption])
 col3.markdown('From the previous charts, it was difficult to observe the change of happiness score over time in terms of both country. This bar chart displays the top 10 countries of each year, colored by region.')
-col3.markdown("From this bar chart, we can see that in the first years, there was a bigger variety of countries from different regions. But as time progresses, ")
+col3.markdown("From this bar chart, we can see that in the first years, there was a bigger variety of countries from different regions, the 10 countries . But as time progresses, more of the top 10 countries are from western europe. By 2021, 9 of the 10 countries are from western europe. I also observed that while western europe countries are dominating the top 10 list, there are still some countries that are able to get onto the list, such as New Zealand, Canada, or Israel, but they are constantly switching over the different years.")
 
-
-col,col2 = st.columns([2,5])
-xoption = col.selectbox('Select the happiness factor to display on the X axis',('Healthy life expectancy at birth', 'Life Ladder',
-       'Social support', 'Generosity',
-       'Log GDP per capita', 'Perceptions of corruption',
-       'Freedom to make life choices'))
-yoption = col.selectbox('Select the happiness factor to display on the Y axis',('Healthy life expectancy at birth', 'Life Ladder',
-       'Social support', 'Generosity',
-       'Log GDP per capita', 'Perceptions of corruption',
-       'Freedom to make life choices'))
-figscatter = px.scatter(merged_table, x= xoption, y = yoption, color = 'Region', hover_name = 'Country name', height = 600, width = 900)
-col2.plotly_chart(figscatter)
-col.markdown('')
-
-
-
-
-
+st.header('Analysis on China')
+china = merged_table[merged_table['Country name']=='China']
+st.markdown('For further analysis, I chose China to do a specific analysis on. Below is all the data for China from 2005-2021.')
+st.write(china)
+st.markdown('First, lets get a description on the data.')
+code10 = china.describe()
+code11 = '''china.describe()'''
+st.code(code11)
+st.write(code10)
+st.markdown('We can see that the happiness score averaged around 5, and there are data from all the years except 2005.')
+st.subheader('Line Graph: Life Ladder')
+col13,col14 = st.columns([3,5])
+code11 = px.line(china, x = 'year',y='Life Ladder', width = 900, height = 600, title = 'China Happiness Score 2005-2021')
+col14.write(code11)
+col13.markdown('From this line graph, we can see that the happiness score has clearly increased over the years, but there are some noticable ups and downs, such as years 2021 and 2009. Also, there were some big increases such as 2009-2011 and 2019-2020.')
+col13.markdown("From my research, I found that the downfall in 2008 might be because of the SiChuan earthquake that was seen as the most destructive and most largescale earthquake in new China history. 69227 people were killed, 374643 were hurt, and 17923 were lost. The direct economic loss was approximately 27,473,539,000 dollars. This was obviously a very destructive event that is very likely to have caused the happiness rate to decrease. However, the damage wasnt permanant, so the happiness rate bounced back the next few years (from what we see in the graph). In 2019, there were some events that contributed to nationalism and major acomplishments. First, it was China's 70th aniversary and there was a lot of celebrations and patriotism. China also finished building a brand new airport in beijing, the capital of the country, and Huawei developed 5G technology. Overall, there were a series of events that are benificial to the economy and natinalism. Then, obviously the downfall from 2020 to 2021 was because China, as well as every other country in the world went into lockdown due to covid-19.")
+st.title('Conclusion')
+st.markdown('In this case study, we looked at the world happiness dataset from 2005-2021, and made comparisons between countries, regions, and happiness variables. First, we found that the three variables most correlated to the ladder score (happiness) are social support, healthy life expectancy at birth, log GDP per capita, while the least correlated happiness factors are year, generosity, and perceptions of corruption. Furthermore, we studied the changes of happiness score in terms of both regions and countries, and found the top countries of each year, of which the region western europe dominated for many years. We also found that countries from the same region tend to have relatively similar ladder scores, and that there are rarely outliers. Lastly, we analyzed China specifically, and found explainations for some unexpected data.')
